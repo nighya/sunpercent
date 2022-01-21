c6430950160f8d8b3bbed787376f09c50c84fca4
rjatkdudgh@naver.com <rjatkdudgh@naver.com>
2022-01-22 02:16:51 +0900

    회원등록 ing

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,6 +1,63 @@
 <template>
   <div class="register pa-6">
-    <h1>register</h1>
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4">
+          <v-card class="elevation-12">
+            <v-toolbar color="purple lighten-3" dark flat>
+              <v-toolbar-title></v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  id="email"
+                  label="ID로 쓰일 email을 입력하세요."
+                  name="email"
+                  prepend-icon="mdi-account"
+                  type="email"
+                  v-model="email"
+                ></v-text-field>
+                <v-text-field
+                  id="nickname"
+                  label="닉네임을 입력하세요."
+                  name="nickname"
+                  prepend-icon="mdi-alert-circle-check"
+                  type="text"
+                  v-model="nickname"
+                ></v-text-field>
+                <v-text-field
+                  id="password"
+                  label="비밀번호"
+                  name="password"
+                  prepend-icon="mdi-lock"
+                  type="password"
+                  v-model="password"
+                ></v-text-field>
+
+                <v-text-field
+                  id="confirm"
+                  label="비밀번호 확인"
+                  name="confirm"
+                  prepend-icon="mdi-alert-circle-check"
+                  type="password"
+                  v-model="confirm"
+                ></v-text-field>
+                <v-radio-group id="gender" v-model="gender" row>
+                  <v-radio label="남" value="male"></v-radio>
+                  <v-radio label="여" value="female"></v-radio>
+                </v-radio-group>
+              </v-form>
+            </v-card-text>
+            <v-card-actions class="justify-center">
+              <v-btn color="purple lighten-3" dark @click="signup"
+                >회원등록</v-btn
+              >
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 <script>
