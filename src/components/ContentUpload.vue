39e87b79dd5537c8153c72f3cf94147a47c5703e
rjatkdudgh@naver.com <rjatkdudgh@naver.com>
2022-01-27 00:30:22 +0900

    dialog 구현시작 ing

--- src/components/ContentUpload.vue
+++ src/components/ContentUpload.vue
@@ -54,6 +54,43 @@
         </v-list-item-group>
       </v-list>
     </v-card>
+    <div class="text-center">
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <!-- <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="red lighten-2"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Click Me
+        </v-btn>
+      </template> -->
+
+      <v-card>
+        <v-card-title class="text-h7 grey lighten-2">
+          이미지가 업로드 되었습니다.
+        </v-card-title>
+        <v-card-text>
+          게시물 확인은 닉네임으로 검색해 보세요. 이전페이지로 이동합니다.
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn 
+            color="primary"
+            text
+            @click="successDialog"
+          >
+           확인
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
   </div>
 </template>
 
@@ -68,7 +105,8 @@ export default {
       previewImage: null,
       progress: 0,
       message: "",
-      imageInfos: []
+      imageInfos: [],
+      dialog:false,
     };
   },
   methods: {
@@ -92,8 +130,8 @@ export default {
           withCredentials: true
         })
         .then(e => {
+          this.dialog = true
           // this.$router.go(-1)
-          this.modal_succeeded;
         })
 
         .catch(err => {
@@ -102,12 +140,11 @@ export default {
           this.currentImage = null;
         });
     },
-    modal_succeeded() {
-      this.$dialog.confirm({
-        text: "test",
-        title: "확인"
-      });
+    successDialog(){
+      this.dialog = false
+      this.$router.go(-1)
     }
+
   }
 };
 </script>
