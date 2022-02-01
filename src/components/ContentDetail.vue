7986f5b8252ad8a780c4f0f7fa5ae9a257924b83
rjatkdudgh@naver.com <rjatkdudgh@naver.com>
2022-02-01 20:48:22 +0900

    contentdetail dialog ing

--- src/components/ContentDetail.vue
+++ src/components/ContentDetail.vue
@@ -9,37 +9,40 @@
 
     <!-- 별점주기 -->
 
-    <v-card v-if="scoredialog == true" class="elevation-16 mx-auto" width="400">
-      <v-card-title class="text-h5">
-        이 사진의 외모는 몇점 입니까?
-      </v-card-title>
-      <v-card-text>
-        게시물 작성자가 점수 공개하기 전까지는 해당 게시물 점수는 게시물
-        작성자만 알 수 있습니다.(7점 만점)
-
-        <div class="text-center mt-12">
-          <v-rating
-            v-model="rating"
-            color="yellow darken-3"
-            background-color="grey darken-1"
-            empty-icon="$ratingFull"
-            half-increments
-            length="7"
-            x-large
-          ></v-rating>
-          <v-card-text>{{ this.rating }} / 7</v-card-text>
-        </div>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions class="justify-space-between">
-        <v-btn text @click="score_cancel">
-          취소
-        </v-btn>
-        <v-btn color="primary" text @click="score_send">
-          점수 보내기
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+    <div class="text-center">
+      <v-dialog v-model="scoredialog" width="500" persistent>
+        <v-card class="elevation-16 mx-auto " width="400">
+          <v-card-title class="text-h5">
+            이 사진의 외모는 몇점 입니까?
+          </v-card-title>
+          <v-card-text>
+            게시물 작성자가 점수 공개하기 전까지는 해당 게시물 점수는 게시물
+            작성자만 알 수 있습니다.(7점 만점)
+            <div class="text-center mt-12">
+              <v-rating
+                v-model="rating"
+                color="yellow darken-3"
+                background-color="grey darken-1"
+                empty-icon="$ratingFull"
+                half-increments
+                length="7"
+                x-large
+              ></v-rating>
+              <v-card-text>{{ this.rating }} / 7</v-card-text>
+            </div>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions class="justify-space-between">
+            <v-btn text @click="score_cancel">
+              취소
+            </v-btn>
+            <v-btn color="primary" text @click="score_send">
+              점수 보내기
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
   </div>
 </template>
 <script>
@@ -47,7 +50,6 @@ export default {
   data() {
     return {
       scoredialog: false,
-      radios: null,
       rating: 1
     };
   },
