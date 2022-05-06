aab333801ad9e96085fa63ecef8fb6259b9bd147
rjatkdudgh@naver.com <rjatkdudgh@naver.com>
2022-05-07 07:33:09 +0900

    검색키워드 자동완성 시작 사진 닉네임 날짜 테이블 형식 시작

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -4,16 +4,74 @@
       class="ml-5 mr-5"
       prepend-icon="mdi-magnify"
       label="search"
-      hint="닉네임을 검색 합니다."
+      hint="닉네임으로 게시물을 검색 합니다."
       persistent-hint
+      @keydown.prevent.enter="search_result"
+      v-model="search_keyword"
     ></v-text-field>
+    <v-row>
+      <v-col
+        v-for="(data, index) in content_data"
+        :key="index"
+        class="d-flex child-flex"
+        cols="4"
+      >
+        <v-img
+          :src="`http://192.168.0.12:4000/${data.image_path}`"
+          :lazy-src="`http://192.168.0.12:4000/${data.image_path}`"
+          aspect-ratio="1"
+          class="grey lighten-2"
+          @click="ContentDetail(data)"
+        >
+          <span class="white--text">{{ data.nickname }}</span>
+          <span class="white--text">({{ data.date }})</span>
+
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular
+                indeterminate
+                color="grey lighten-5"
+              ></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
+import http from "../http/http";
+
 export default {
   data() {
-    return {};
+    return {
+      content_data: "",
+      content_no_data_msg: "",
+      search_keyword: null
+    };
+  },
+  methods: {
+    async search_result() {
+      const nicknameObj = { nickname: this.search_keyword };
+      console.log("nicknameObj :  " + nicknameObj);
+      const response = await http.post(`/content/search`, nicknameObj, {
+        withCredentials: true
+      });
+      this.content_data = response.data;
+      if (response.data.msg == "No Data") {
+        this.content_no_data_msg = "검색 결과가 없습니다.";
+      }
+    },
+    ContentDetail(data) {
+      this.$router.push({
+        name: "ContentDetail",
+        params: {
+          content_uid: data.content_uid,
+          datas: data
+        }
+      });
+    }
   }
 };
 </script>
