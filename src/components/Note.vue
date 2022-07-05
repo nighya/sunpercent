c9edb07afcb221b78d14e26581771fe88b6ee29d
rjatkdudgh@naver.com <rjatkdudgh@naver.com>
2022-07-06 00:02:55 +0900

    ing

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,8 +1,25 @@
 <template>
-<h1>Note</h1>
+  <v-card>
+    <v-tabs color="deep-purple accent-4" right>
+      <v-tab>쪽지보내기</v-tab>
+      <v-tab>받은쪽지</v-tab>
+      <v-tab>보낸쪽지</v-tab>
+
+      <v-tab-item v-for="n in 3" :key="n">
+        <v-container fluid>
+          <v-row>
+            <v-col>
+              <h1 v-if="n == 1">1번텝</h1>
+              <h1 v-if="n == 2">2번텝</h1>
+              <h1 v-if="n == 3">3번텝</h1>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-tab-item>
+    </v-tabs>
+  </v-card>
 </template>
+
 <script>
-export default {
-    
-}
+export default {};
 </script>
